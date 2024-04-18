82f2f921ad2cec73d8731d15eed800c2b189d568
Ravi <ravitejayadlapalli@gmail.com>
2024-04-18 12:51:25 +0530

    Added reset() method and high score logic.

--- main.py
+++ main.py
@@ -35,14 +35,20 @@ while game_is_on:
 
     #detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
+        # game_is_on = False
+        # scoreboard.game_over()
 
     #detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
+            # game_is_on = False
+            # scoreboard.game_over()
 
 
 screen.exitonclick()
